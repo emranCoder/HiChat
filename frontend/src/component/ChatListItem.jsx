2864778661dcfdac29e995ae2d6ff539f17c5b4b
emranCoder <shaheyalam74@gmail.com>
2023-10-23 22:32:15 +0600

    some unwanted import removed

--- frontend/src/component/ChatListItem.jsx
+++ frontend/src/component/ChatListItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { fetchMessages } from "../redux/messSlice";
 import utility from "../redux/storeData";
