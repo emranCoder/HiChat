de1de1d7e917fdb5dac169c8da42fe66c0b511ff
emranCoder <shaheyalam74@gmail.com>
2023-10-25 17:53:35 +0600

    login ui created

--- frontend/src/component/LoginComponent.js
+++ frontend/src/component/LoginComponent.js
@@ -2,46 +2,59 @@ import React from 'react'
 
 export default function LoginComponent() {
     return (
-        <div>
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">
-                        Email address
-                    </label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"
-                    />
-                    <div id="emailHelp" className="form-text">
-                        We'll never share your email with anyone else.
+        <div className='container '>
+            <div className="row">
+                <div className="col-md-6 col-lg-6">
+                    <div className="logo-box p-5" style={{ marginTop: "100px " }}>
+                        <h3>HiChat</h3>
+                        <p>Connecting Conversations, One Message at a Time.</p>
                     </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="exampleInputPassword1"
-                    />
+                <div className="col-md-6 col-lg-6">
+                    <form className='login-form rounded-5 p-5 mt-5' >
+                        <h3 className='form-title m-5 mt-0'>Login Form</h3>
+                        <div className="mb-3">
+                            <input
+                                placeholder='Email, Mobile or Username'
+                                type="email"
+                                className="form-control rounded-pill"
+                                id="email"
+                                aria-describedby="emailHelp"
+                            />
+                            <span id="emailError" className="form-text d-none">
+                                We'll never share your email with anyone else.
+                            </span>
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                placeholder='Password'
+                                type="pwd"
+                                className="form-control rounded-pill"
+                                id="pwd"
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <a href="#section" className='forget-pwd'>Forget Password?</a>
+                        </div>
+                        <div className="mb-3 form-check">
+                            <input
+                                type="checkbox"
+                                className="form-check-input"
+                                id="rmm"
+                            />
+                            <label className="form-check-label" htmlFor="exampleCheck1">
+                                Remember Me
+                            </label>
+                        </div>
+                        <button type="submit" className="btn btn-primary rounded-pill">
+                            SIGN IN
+                        </button>
+                        <div className="m-3 ">
+                            <a href="#section" className='forget-pwd'>Create a new account</a>
+                        </div>
+                    </form>
                 </div>
-                <div className="mb-3 form-check">
-                    <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="exampleCheck1"
-                    />
-                    <label className="form-check-label" htmlFor="exampleCheck1">
-                        Check me out
-                    </label>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Submit
-                </button>
-            </form>
-        </div>
+            </div>
+        </div >
     )
 }
